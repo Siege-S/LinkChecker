04dffa4ea497f3d7fe2d999df8da09eb3a825be0
Siege-S <sanchezchristianjoshua@gmail.com>
2024-05-18 09:44:07 +0800

    fixed multiple scan of URL

--- app/src/main/java/com/example/smslinkchecker/SmsListener.java
+++ app/src/main/java/com/example/smslinkchecker/SmsListener.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.telephony.SmsMessage;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 // Regex
@@ -12,6 +13,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SmsListener extends BroadcastReceiver {
+
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -29,12 +31,21 @@ public class SmsListener extends BroadcastReceiver {
                         String msgBody = msgs[i].getMessageBody();
 
 //                         Toast.makeText(context, "From: "+ msg_from+" , Body: "+msgBody, Toast.LENGTH_SHORT).show();
+
                         // Check if sms message contains Links using regex
-                        Pattern URL_PATTERN = Pattern.compile("\\b(?:https?://)?(?:www\\.)?\\w+\\.\\w{2,}(?:/[-a-zA-Z0-9@:%_\\+.~#?&//=]*)?\\b");
+                        // 1st regex pattern (testing)
+//                        Pattern URL_PATTERN = Pattern.compile("\\b(?:https?://)?(?:www\\.)?\\w+\\.\\w{2,}(?:/[-a-zA-Z0-9@:%_\\+.~#?&//=]*)?\\b");
+                        // 2nd regex pattern (testing)
+//                        Pattern URL_PATTERN = Pattern.compile("https?:\\/\\/(?:www\\.)?(?:\\w+-?)+\\.\\w+(?:\\/\\w+)*\n");
+                        // 3rd regex pattern (testing -
+                        Log.v("URLs", "From: "+ msg_from+" , Body: "+msgBody);
+                        Pattern URL_PATTERN = Pattern.compile("\\b(?:https?://)?(?:www\\.)?([\\w-]+(?:\\.[\\w-]+)*)\\.[a-z]{2,}(?:/[\\w-]+(?:/[^ \\n]*+)*)?\\b");
                         Matcher URL_MATCHER = URL_PATTERN.matcher(msgBody);
-                        for (int j = 0; j < URL_MATCHER.groupCount(); j++) {
-                            String url = URL_MATCHER.group(j);
+                        while (URL_MATCHER.find()) {
+                            String url = URL_MATCHER.group();
+                            // Do something with the URL (e.g., Toast or Log)
                             Toast.makeText(context, "URL Detected: " + url, Toast.LENGTH_LONG).show();
+                            Log.v("URL", url);
                         }
                     }
                 } catch (Exception e) {
