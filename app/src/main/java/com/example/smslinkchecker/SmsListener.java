b705548d3a0062015e1b8a5604a4ca28c876c684
Siege-S <sanchezchristianjoshua@gmail.com>
2024-11-17 11:37:17 +0800

    Modified Smslistener

--- app/src/main/java/com/example/smslinkchecker/SmsListener.java
+++ app/src/main/java/com/example/smslinkchecker/SmsListener.java
@@ -56,122 +56,110 @@ import org.json.JSONObject;
 
 public class SmsListener extends BroadcastReceiver {
     private static final String CHANNEL_ID = "1001";
-    private static final int NOTIFICATION_ID = 123;
     private static final String API_KEY = BuildConfig.VT_API_KEY;
     private static final String ss_API_KEY = BuildConfig.SS_API_KEY;
     private static final int POLLING_INTERVAL_MS = 10000; // 10 seconds
 
+    // Modified smslistener (puro error kase)
     @Override
     public void onReceive(Context context, Intent intent) {
+        if (!"android.provider.Telephony.SMS_RECEIVED".equals(intent.getAction())) return;
+
         DBHelper dbHelper = new DBHelper(context);
+        SmsMessage[] messages = extractSmsMessages(intent);
+        if (messages == null) return;
 
-        if (intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")) {
-            Bundle bundle = intent.getExtras();
-            SmsMessage[] msgs;
-            String msg_from;
-            if (bundle != null) {
-                try {
-                    Object[] pdus = (Object[]) bundle.get("pdus");
-                    msgs = new SmsMessage[pdus.length];
-                    for (int i = 0; i < msgs.length; i++) {
-                        msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
-                        msg_from = msgs[i].getOriginatingAddress();
-                        String msgBody = msgs[i].getMessageBody();
-                        Log.v("URLs", "From: " + msg_from + " , Body: " + msgBody);
+        for (SmsMessage message : messages) {
+            String msgFrom = message.getOriginatingAddress();
+            String msgBody = message.getMessageBody();
+            Log.v("URLs", "From: " + msgFrom + " , Body: " + msgBody);
 
-                        // List to store detected URLs
-                        List<String> urls = new ArrayList<>();
-                        String detectedUrl;
-                        Pattern URL_PATTERN = Patterns.WEB_URL; // Regular expression pattern to detect Web URLs
-                        Matcher URL_MATCHER = URL_PATTERN.matcher(msgBody);
-                        while (URL_MATCHER.find()) {
-                            detectedUrl = URL_MATCHER.group();
-                            urls.add(detectedUrl);  // Add URL to list
-                            Toast.makeText(context, "URL Detected: " + detectedUrl, Toast.LENGTH_LONG).show();
-                            Log.v("URL", detectedUrl);
-                        }
+            List<String> urls = extractUrlsFromMessage(msgBody, context);
 
-                        if (!MessageFragment.isInternetConnected(context)) {
-                            Toast.makeText(context, "No Internet Connection", Toast.LENGTH_SHORT).show();
-                            int notificationID = 0;
-                            for (String url : urls) {
-                                notificationID++;
-                                if (dbHelper.insertOfflineTbl(url, msg_from)) {
-                                    noInternet(context, url, notificationID);
-                                }
-                            }
-                        } else {
-                            // Create a single ExecutorService for processing URLs
-                            ExecutorService executorService = Executors.newSingleThreadExecutor();
-                            try {
-                                // Process each URL in the list
-                                for (String url : urls) {
-                                    int notifID = 0;
-                                    createNotification(context, "URL Detected in SMS message", url, notifID);
-                                    notifID++;
-                                    String finalMsg_from = msg_from;
-                                    executorService.execute(() -> {
-                                        try {
-                                            // Method Call API return API URL
-                                            String apiUrl = SnapshotmachineAPI(url);
-                                            // Asynchronously scan URL
-                                            String analysisId = processUrls(context, url);
-
-                                            if (analysisId != null) {
-                                                // Asynchronously get analysis result
-                                                String analysisResultJSON = getAnalysis(analysisId);
-
-                                                if (analysisResultJSON != null) {
-                                                    // Download image and process the analysis result on a background thread
-                                                    Bitmap bitmap;
-                                                    InputStream in = new URL(apiUrl).openStream();
-                                                    bitmap = BitmapFactory.decodeStream(in);
-                                                    byte[] image = getBitmapAsByteArray(bitmap);
-
-                                                    // Notify the user with the analysis result on the main thread
-                                                    new Handler(Looper.getMainLooper()).post(() -> {
-                                                        String analysis = NotifyResult(context, url, analysisResultJSON);
-
-                                                        // Insert into database in SQLite
-                                                        dbHelper.insertData(url, finalMsg_from, apiUrl, analysis, image, analysisResultJSON);
-                                                    });
-                                                } else {
-                                                    showRetryNotification(context, url, finalMsg_from);
-                                                    System.out.println("Failed to get analysis result.");
-                                                }
-                                            } else {
-                                                showRetryNotification(context, url, finalMsg_from);
-                                                Log.e("Error", "Failed to processURL result.");
-                                            }
-                                        } catch (IOException e) {
-                                            System.out.println("Error in VirusTotal:" + e.getMessage());
-                                            showRetryNotification(context, url,finalMsg_from);
-                                            e.printStackTrace();
-                                        } catch (NoSuchAlgorithmException e) {
-                                            Log.e("Error", "Failed to scan URL.");
-                                            showRetryNotification(context, url,finalMsg_from);
-                                            throw new RuntimeException(e);
-                                        } catch (JSONException e) {
-                                            Log.e("VirusTotal Error", e.getMessage(), e);
-                                            showRetryNotification(context, url,finalMsg_from);
-                                            throw new RuntimeException(e);
-                                        }
-                                    });
-                                }
-                            } finally {
-                                // Shutdown the executor service after all tasks are submitted
-                                executorService.shutdown();
-                            }
-                        }
-                    }
-                } catch (Exception e) {
-                    Log.e("SMS Processing Error", e.getMessage(), e);
-                    e.printStackTrace();
-                }
+            if (!MessageFragment.isInternetConnected(context)) {
+                handleOfflineUrls(context, urls, msgFrom, dbHelper);
+            } else {
+                processUrlsWithInternet(context, urls, msgFrom, dbHelper);
             }
         }
     }
 
+    private SmsMessage[] extractSmsMessages(Intent intent) {
+        Bundle bundle = intent.getExtras();
+        if (bundle == null) return null;
+
+        Object[] pdus = (Object[]) bundle.get("pdus");
+        SmsMessage[] messages = new SmsMessage[pdus.length];
+        for (int i = 0; i < pdus.length; i++) {
+            messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+        }
+        return messages;
+    }
+
+    private List<String> extractUrlsFromMessage(String msgBody, Context context) {
+        List<String> urls = new ArrayList<>();
+        Matcher matcher = Patterns.WEB_URL.matcher(msgBody);
+        while (matcher.find()) {
+            String detectedUrl = matcher.group();
+            urls.add(detectedUrl);
+            Toast.makeText(context, "URL Detected: " + detectedUrl, Toast.LENGTH_LONG).show();
+            Log.v("URL", detectedUrl);
+        }
+        return urls;
+    }
+
+    private void handleOfflineUrls(Context context, List<String> urls, String msgFrom, DBHelper dbHelper) {
+        Toast.makeText(context, "No Internet Connection", Toast.LENGTH_SHORT).show();
+        int notificationID = 0;
+        for (String url : urls) {
+            notificationID++;
+            if (dbHelper.insertOfflineTbl(url, msgFrom)) {
+                noInternet(context, url, notificationID);
+            }
+        }
+    }
+
+    private void processUrlsWithInternet(Context context, List<String> urls, String msgFrom, DBHelper dbHelper) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        try {
+            for (String url : urls) {
+                createNotification(context, "URL Detected in SMS message", url, 100);
+                executorService.execute(() -> processSingleUrl(context, url, msgFrom, dbHelper));
+            }
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    private void processSingleUrl(Context context, String url, String msgFrom, DBHelper dbHelper) {
+        try {
+            String apiUrl = SnapshotmachineAPI(url);
+            String analysisId = processUrls(context, url);
+            if (analysisId != null) {
+                handleAnalysisResult(context, url, msgFrom, apiUrl, analysisId, dbHelper);
+            } else {
+                showRetryNotification(context, url, msgFrom);
+            }
+        } catch (Exception e) {
+            Log.e("Error", "Failed to process URL", e);
+            showRetryNotification(context, url, msgFrom);
+        }
+    }
+
+    private void handleAnalysisResult(Context context, String url, String msgFrom, String apiUrl, String analysisId, DBHelper dbHelper) throws IOException, JSONException, NoSuchAlgorithmException {
+        String analysisResultJSON = getAnalysis(analysisId);
+        if (analysisResultJSON != null) {
+            Bitmap bitmap = BitmapFactory.decodeStream(new URL(apiUrl).openStream());
+            byte[] image = getBitmapAsByteArray(bitmap);
+
+            new Handler(Looper.getMainLooper()).post(() -> {
+                String analysis = NotifyResult(context, url, analysisResultJSON);
+                dbHelper.insertData(url, msgFrom, apiUrl, analysis, image, analysisResultJSON);
+            });
+        } else {
+            showRetryNotification(context, url, msgFrom);
+        }
+    }
 
     public void showRetryNotification(Context context, String url, String sender) {
         Intent retryIntent = new Intent(context, RetryReceiver.class);
@@ -266,22 +254,22 @@ public class SmsListener extends BroadcastReceiver {
             int harmless = jsonObject.getJSONObject("data").getJSONObject("attributes").getJSONObject("stats").getInt("harmless");
 
             if(malicious > 0 && suspicious > 0){ // 3 if malicious and suspicious
-                notificationManager.notify(3, builder3.build()); // Notification ID 3
+                notificationManager.notify(100, builder3.build()); // Notification ID 3
                 System.out.println("Link is Malicious and Suspicious" + url);
                 return "3";
             }
             else if(malicious > 0){ // 1 if malicious
-                notificationManager.notify(2, builder2.build()); // Notification ID 2
+                notificationManager.notify(100, builder2.build()); // Notification ID 2
                 System.out.println("Link is Malicious" + url);
                 return "1";
             }
             else if(suspicious > 0){ // 2 if suspicious
-                notificationManager.notify(1, builder1.build()); // Notification ID 1
+                notificationManager.notify(100, builder1.build()); // Notification ID 1
                 System.out.println("Link is Suspicious/Phishing" + url);
                 return "2";
             }
             else { // 0 if harmless
-                notificationManager.notify(4, builder4.build()); // Notification ID 4
+                notificationManager.notify(100, builder4.build()); // Notification ID 4
                 return "0";
             }
 
@@ -449,7 +437,12 @@ public class SmsListener extends BroadcastReceiver {
             return null;
         }
 
-        OkHttpClient client = new OkHttpClient();
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .build();
+
         Request request = new Request.Builder()
                 .url("https://www.virustotal.com/api/v3/analyses/" + analysisId)
                 .get()
@@ -523,7 +516,7 @@ public class SmsListener extends BroadcastReceiver {
                 Log.e("TestListener", "Attempt " + (i + 1) + " failed", e);
                 if (i == retryCountNew - 1) {
                     invalidURL(context, url);
-                    throw e;  // Final attempt failed, propagate the exception
+                    throw e;  // Final attempt failed, exception
                 }
             }
         }
